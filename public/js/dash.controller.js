46865f74910677da6abf8f4be0a720f1aed0dc61
Patrick Shaughnessy <pshaughn86@gmail.com>
2016-05-12 13:53:55 -0700

    final pre-judging

--- public/js/dash.controller.js
+++ public/js/dash.controller.js
@@ -6,34 +6,39 @@
 
 	function DashController($scope, mySocket, MainService){
 
-        $scope.selectedStation = 'mont';
-        $scope.selectedCar = 'A';
-        $scope.options = {
-            chart: {
-                type: 'pieChart',
-                x: function(d){return d.key;},
-                y: function(d){return d.y;},
-                showLabels: false,
-                showLegend: false,
-                duration: 500,
-                labelSunbeamLayout: true
-            }
-        };
-
-		function updateSelection() {
-            updateGraph();
+    $scope.selectedStation = 'mont';
+    $scope.selectedCar = 'A';
+    $scope.options = {
+        chart: {
+            type: 'pieChart',
+            x: function(d){return d.key;},
+            y: function(d){return d.y;},
+            showLabels: false,
+            showLegend: false,
+            duration: 500,
+            labelSunbeamLayout: true
         }
+    };
 
-        mySocket.on('predix', function(data){
-        	MainService.updatePredix(data);
-        	updateGraph();
-        });
+		// $scope.dataSelected = MainService.data;
 
-        function updateGraph() {
-        	$scope.data = MainService.data;
-            $scope.dataSelected = $scope.data[$scope.selectedStation][$scope.selectedCar];
-        }
-        updateGraph();
+
+
+		// function updateSelection() {
+    //   updateGraph();
+    // }
+
+    // mySocket.on('predix', function(data){
+    // 	MainService.updatePredix(data);
+    // 	updateGraph();
+    // });
+
+    // function updateGraph() {
+    // 	$scope.data = MainService.data;
+		// 	console.log('here', $scope.selectedStation, $scope.selectedCar);
+    //   $scope.dataSelected = $scope.data[$scope.selectedStation][$scope.selectedCar];
+    // }
+    // updateGraph();
 	}
 
 })();
